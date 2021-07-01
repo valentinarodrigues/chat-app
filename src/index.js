bf4bcba48f1624c21f00cb0f76837828c50aabe1
Valentina Rodrigues <valentinarodrigues93@gmail.com>
2021-07-01 12:17:00 +0530

    use io library

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 const path = require('path')
 const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
+
 
 
 const app = express()
+// created this becasue it is needed in socketio express does it behind the scenes
 const server = http.createServer(app)
-
+const io = socketio(server)
 const port = process.env.PORT || 3001
 
 const publicPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicPath))
-
+io.on('connection', ()=>{
+    console.log('Here is io ')
+})
 server.listen(port, ()=>{
     console.log('Server is up at port ', port)
 })
