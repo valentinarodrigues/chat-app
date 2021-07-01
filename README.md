ec1f983584f85a174d738f5515740ce30f2b4884
Valentina Rodrigues <valentinarodrigues93@gmail.com>
2021-07-01 12:09:58 +0530

    refactor express

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # chat-app
+
+Web sockets!!
+
+- Allows full duplex communication (bidirectional communication)
+- Persistent connection between client and server 
+- Separate protocol than HTTP
+
+
+Documentation
+- socket.io
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
 
+
 const app = express()
+const server = http.createServer(app)
 
 const port = process.env.PORT || 3001
 
@@ -9,6 +12,6 @@ const publicPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicPath))
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log('Server is up at port ', port)
 })
