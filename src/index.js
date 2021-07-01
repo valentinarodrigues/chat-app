105a5a1d08167cd244689f7caf6a8b3a9040b488
Valentina Rodrigues <valentinarodrigues93@gmail.com>
2021-07-01 11:56:11 +0530

    express setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 3001
+
+const publicPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicPath))
+
+app.listen(port, ()=>{
+    console.log('Server is up at port ', port)
+})
