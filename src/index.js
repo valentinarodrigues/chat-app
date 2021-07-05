c7e6df9a8275589e8f59f81c3534c33b677b2a11
Valentina Rodrigues <valentinarodrigues93@gmail.com>
2021-07-05 14:21:20 +0530

    profane word check

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
-const { createWebSocketStream } = require('ws')
+const Filter = require('bad-words')
 
 const app = express()
 // created this becasue it is needed in socketio express does it behind the scenes
@@ -13,23 +13,28 @@ const port = process.env.PORT || 3001
 const publicPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicPath))
-const message = 'Welcome'
+// const message = 'Welcome'
+const message = 'New member has joined'
 io.on('connection', (socket) => {
     console.log('Client is connected ')
 
     // socket.emit('sendWelcome', message) // emits to self as well
     // broadcast to all except self
-    socket.broadcast.emit('sendWelcome', 'New member has joined')
+    socket.broadcast.emit('sendWelcome', message)
     socket.on('sendData', (receivedData, callback) => {
-        io.emit('sendAllClients', receivedData)
-        callback('Received!')
+        const filter = new Filter()
+        if(!filter.isProfane(receivedData)){
+            return callback('Profane words are not allowed')
+        }
+        io.emit('sendClients', receivedData)
+        callback()
     })
-    socket.on('sendLocation', (data)=>{
-        console.log(data)
-        socket.emit('sendAllClients', `http://google.com/maps?q=${data.latitude},${data.longitude}`,()=>{
-            console.log('Message was delivered');
-        })
 
+    socket.on('sendLocation', (data, callback)=>{
+        socket.emit('sendAllClients', `http://google.com/maps?q=${data.latitude},${data.longitude}`,()=>{
+            console.log('Location shared');
+        })
+        callback()
     })
     // when a client gets disconnected we just need to write an event listener since the event disconnect is an inbuilt event in socket.io
     socket.on('disconnect', ()=>{
